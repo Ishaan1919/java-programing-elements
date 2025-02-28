af6fef860e9af322a23b852d4ed0e7d57def677f
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-02-28 10:56:25 +0530

    [Ishaan] Program to find the average of sam's marks in PCM

---
+++ AverageMarks.java
@@ -0,0 +1,9 @@
+public class AverageMarks{
+	public static void main(String[] args){
+		int maths = 94;
+		int chemistry = 96;
+		int physics = 95;
+		float average = (maths+chemistry+physics)/3;
+		System.out.print("Sam’s average mark in PCM is " + average);
+	}
+}
