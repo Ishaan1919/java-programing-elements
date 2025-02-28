e14cc813c63c276811f4bf81e957035a446bc421
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-02-28 11:04:13 +0530

    MOD: Converting kilometers to miles

--- ConvertToMiles.java
+++ ConvertToMiles.java
@@ -1,7 +1,7 @@
 public class ConvertToMiles{
 	public static void main(String[] args){
-		float kilometers = 10.8;
-		float oneMile = 1.6;
+		float kilometers = (float)10.8;
+		float oneMile = (float)1.6;
 		float miles = kilometers*oneMile;
 		System.out.print("The distance 10.8 km in miles is " + miles);
 	}
